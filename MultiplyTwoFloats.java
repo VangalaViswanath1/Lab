ff02c04a3274c6a16051e2397973d647810cd82f
Vangala Viswanath <98110836+VangalaViswanath1@users.noreply.github.com>
2022-06-23 13:52:02 +0530

    Multliply Two Floats

---
+++ MultiplyTwoFloats.java
@@ -0,0 +1,8 @@
+
+class MultiplyTwoFloats{
+         public static void main(String args[]){
+	float num1 = 67.99009f;
+	float num2 = 2.22f;
+	System.out.println("Multiply two floats : "+ num1 * num2);
+	}
+}
