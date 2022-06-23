52781d9cc8647639bc3a7422b889bb33ec774c35
Vangala Viswanath <98110836+VangalaViswanath1@users.noreply.github.com>
2022-06-23 13:53:35 +0530

    Multiply Two numbers

---
+++ MultiplyTwoNums.java
@@ -0,0 +1,11 @@
+
+class MultiplyTwoNums{
+	public static void main(String args[]){
+	int num1 = 62;
+	int num2 = 62;
+	int num3 = num1 * num2;
+	System.out.println("Multiply two numbers is"+ num3);
+
+}
+	
+}
