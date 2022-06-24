96452b635dcd04fe46091f8c003ae17f9907dbbb
Vangala Viswanath <98110836+VangalaViswanath1@users.noreply.github.com>
2022-06-24 11:01:17 +0530

    Update UserInput.java

--- UserInput.java
+++ UserInput.java
@@ -1,9 +1,9 @@
 
-import java.util.Scanner;
+import java.util.Scanner;				//imported scanner package here
 class UserInput{
 	public static void main(String args[])
 	{
-		Scanner sc = new Scanner(System.in);
-		System.out.println(sc.nextLine());
+		Scanner sc = new Scanner(System.in);	// use the scanner package here
+		System.out.println(sc.nextLine());	// printing the User input line here...
 	}
 }
