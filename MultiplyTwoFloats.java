ceb9409d409a23e3a75575b334959d74d339f7ea
Vangala Viswanath <98110836+VangalaViswanath1@users.noreply.github.com>
2022-06-24 10:55:11 +0530

    Update MultiplyTwoFloats.java

--- MultiplyTwoFloats.java
+++ MultiplyTwoFloats.java
@@ -1,8 +1,8 @@
 
 class MultiplyTwoFloats{
          public static void main(String args[]){
-	float num1 = 67.99009f;
-	float num2 = 2.22f;
-	System.out.println("Multiply two floats : "+ num1 * num2);
+	float num1 = 67.99009f;						// first float number
+	float num2 = 2.22f;						//second float number
+	System.out.println("Multiply two floats : "+ num1 * num2);	// here multiplied two float numbers
 	}
 }
