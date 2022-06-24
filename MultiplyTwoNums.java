1560e1a7a6bb9926e8a6fd6abb0fba10f20885f1
Vangala Viswanath <98110836+VangalaViswanath1@users.noreply.github.com>
2022-06-24 10:58:52 +0530

    Update MultiplyTwoNums.java

--- MultiplyTwoNums.java
+++ MultiplyTwoNums.java
@@ -1,10 +1,10 @@
 
 class MultiplyTwoNums{
 	public static void main(String args[]){
-	int num1 = 62;
-	int num2 = 62;
-	int num3 = num1 * num2;
-	System.out.println("Multiply two numbers is"+ num3);
+	int num1 = 62;						// first number
+	int num2 = 62;						//second number
+	int num3 = num1 * num2;					// assigned first number and second number to third number and multipled num1 with num2
+	System.out.println("Multiply two numbers is"+ num3);	// here is showing the result of num3..
 
 }
 	
